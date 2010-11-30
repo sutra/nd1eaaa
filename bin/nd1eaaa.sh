21533b56ea9d5eafd48a568d6865adff29392527
Sutra Zhou <zhoushuqun@gmail.com>
2010-11-30 23:36:10 +0800

    Add another ssh host.

--- bin/nd1eaaa.sh
+++ bin/nd1eaaa.sh
@@ -1,23 +1,34 @@
 #! /bin/bash
 PATH=./bin
-url="http://www.ssh2proxy.com/"
-user="ssh2proxy.com"
-host=173.0.51.222
-lport=7070
-pw=$( curl -s "$url" | sed '/服务器密码/!d;s/^.*value="//;s/".*$//' )
+urls=("http://www.ssh2proxy.com/" "http://blog.paying.org.ru/")
+users=("ssh2proxy.com" "paying.org.ru")
+hosts=("173.0.51.222" "196.46.191.163")
 
-  for (( i=1 ; i<=10 ; i++ ))
-      do
-          if echo "$pw" | grep '服务器人数过多，请稍候再来'
-              then
-                 sleep 60
-                 pw=$(curl -s "$url" | sed '/服务器密码/!d;s/^.*value="//;s/".*$//')
-                 [ $i -gt 9 ] && { echo "已經嘗試連接十次.請稍後再試." ;
-                                  exit 5
-                 }
-              else
-                  break
-          fi
-      done
+connect() {
+	url=$1
+	user=$2
+	host=$3
+	lport=7070
+	pw=$( curl -s "$url" | sed '/服务器密码/!d;s/^.*value="//;s/".*$//' )
 
-plink "$host" -N -ssh -2 -P 22 -l "$user" -pw "$pw" -D $lport -v
+	for (( i=1 ; i<=10 ; i++ )); do
+		if echo "$pw" | grep '服务器人数过多，请稍候再来'; then
+			sleep 60
+			pw=$(curl -s "$url" | sed '/服务器密码/!d;s/^.*value="//;s/".*$//')
+			[ $i -gt 9 ] && {
+				echo "已經嘗試連接十次.請稍後再試." ;
+				exit 5
+			}
+		else
+			break
+		fi
+	done
+
+	plink "$host" -N -ssh -2 -P 22 -l "$user" -pw "$pw" -D $lport -v
+}
+
+for (( i = 0; i < ${#urls[@]}; i++ )); do
+	connect ${urls[$i]} ${users[$i]} ${hosts[$i]}
+done
+
+
