4b57510e8d67a7a6181dc758f932a51a2f75344f
Sutra Zhou <zhoushuqun@gmail.com>
2010-12-01 00:32:12 +0800

    Fix nd1eaaa.sh.

--- bin/nd1eaaa.sh
+++ bin/nd1eaaa.sh
@@ -3,32 +3,38 @@ PATH=./bin
 urls=("http://www.ssh2proxy.com/" "http://blog.paying.org.ru/")
 users=("ssh2proxy.com" "paying.org.ru")
 hosts=("173.0.51.222" "196.46.191.163")
+lport=7070
 
-connect() {
-	url=$1
-	user=$2
-	host=$3
-	lport=7070
-	pw=$( curl -s "$url" | sed '/服务器密码/!d;s/^.*value="//;s/".*$//' )
+echo '尝试获得登录帐号……'
+for (( t = 1; t <= 10; t++ )); do
+	echo "第${t}次尝试……"
+	for (( i = 0; i < ${#urls[@]}; i++ )); do
+		url=${urls[$i]}
+		user=${users[$i]}
+		host=${hosts[$i]}
 
-	for (( i=1 ; i<=10 ; i++ )); do
-		if echo "$pw" | grep '服务器人数过多，请稍候再来'; then
-			sleep 60
-			pw=$(curl -s "$url" | sed '/服务器密码/!d;s/^.*value="//;s/".*$//')
-			[ $i -gt 9 ] && {
-				echo "已經嘗試連接十次.請稍後再試." ;
-				exit 5
-			}
+		echo "正在从 ${url} 获取密码信息……"
+
+		pw=$( curl -s "$url" | sed '/服务器密码/!d;s/^.*value="//;s/".*$//' )
+
+		if [ -z "$pw" ]; then
+			echo "从 ${url} 获取密码页面失败"
+			continue
+		elif echo "$pw" | grep '服务器人数过多，请稍候再来'; then
+			echo '服务器人数过多'
+			continue
 		else
-			break
+			echo "获得帐号成功：${host}, ${user}, ${pw}"
+			break 2
 		fi
 	done
 
-	plink "$host" -N -ssh -2 -P 22 -l "$user" -pw "$pw" -D $lport -v
-}
-
-for (( i = 0; i < ${#urls[@]}; i++ )); do
-	connect ${urls[$i]} ${users[$i]} ${hosts[$i]}
+	sleep 60
+	[ $t -gt 9 ] && {
+		echo "已經嘗試連接十次.請稍後再試." ;
+		exit 5
+	}
 done
 
-
+echo "建立隧道到：${host}……"
+plink "$host" -N -ssh -2 -P 22 -l "$user" -pw "$pw" -D $lport -v
